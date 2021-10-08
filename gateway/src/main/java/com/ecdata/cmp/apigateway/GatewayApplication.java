ccb48496eb1d8a94098a14b95cb4da6194a92bf9
xujiang110308 <vincent_110308@126.com>
2021-10-08 15:11:18 +0800

    first commit

---
+++ gateway/src/main/java/com/ecdata/cmp/apigateway/GatewayApplication.java
@@ -0,0 +1,34 @@
+package com.ecdata.cmp.apigateway;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author ：xuj
+ * @date ：Created in 2021/10/8 14:37
+ * @modified By：
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+    /**
+     * @param args args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    /**
+     * @param applicationName applicationName
+     * @return configurer
+     */
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+        return registry -> registry.config().commonTags("application", applicationName);
+    }
+}
